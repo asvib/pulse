d4dd2a8b875264da0ee9364bcd135775f9daa7ae
Andrei <thebestnewlogin@yandex.ru>
2024-03-25 16:50:02 +0300

    made tabs using jQuery

--- src/js/script.js
+++ src/js/script.js
@@ -9,8 +9,20 @@ const slider = tns({
 
   document.querySelector('.prev').addEventListener('click', function() {
     slider.goTo('prev');
-  })
+  });
 
   document.querySelector('.next').addEventListener('click', function() {
     slider.goTo('next');
-  })
+  });
+
+  (function($) {
+    $(function() {
+      
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+      
+    });
+    })(jQuery);
