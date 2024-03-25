574efd09ed637a7a3209114f85cbb89b047000f5
Andrei <thebestnewlogin@yandex.ru>
2024-03-25 22:23:31 +0300

    the map section was added

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 const slider = tns({
-    container: '.carousel__inner',
+  container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
     autoplay: false,
@@ -15,14 +15,27 @@ const slider = tns({
     slider.goTo('next');
   });
 
-  (function($) {
-    $(function() {
-      
+(function($) {
+  $(function() {
+  
       $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-      });
-      
     });
-    })(jQuery);
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+      
+});
+})(jQuery);
